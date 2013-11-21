ebd322f6257ddaf7ab29f5b95ecafadf9bfe4f91
Henri Hirvonen <henri.hirvonen@wunderkraut.com>
2013-11-21 21:26:18 +0200

    Simple threading

--- spiralbit.py
+++ spiralbit.py
@@ -2,18 +2,58 @@ import json
 import urllib
 import urllib2
 import time
+import threading
+
+class trader (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        print "Starting " + self.name
+        #print_time(self.name, self.counter, 5)
+        print "Exiting " + self.name
+
+class monitor (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        print "Starting " + self.name
+        pullPrice()
+        print "Exiting " + self.name
 
 def pullPrice():
-    url = "https://www.bitstamp.net/api/ticker"
+    while True:
+        url = "https://www.bitstamp.net/api/ticker"
+        #data = urllib.urlencode(parameters)
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        price = json.load(response)
+        time.sleep(10)
 
-    #data = urllib.urlencode(parameters)
-    req = urllib2.Request(url)
-    response = urllib2.urlopen(req)
+threadLock = threading.Lock()
+threads = []
 
-    json_object = json.load(response)
+# Create new threads
+monitor1 = monitor(1, "Monitor-1")
+trader1 = trader(1, "Trader-1")
+trader2 = trader(2, "Trader-2")
 
-    print json_object
+# Start new Threads
+monitor1.start()
+trader1.start()
+trader2.start()
 
-while True:
-    pullPrice()
-    time.sleep(10)
+
+
+# Add threads to thread list
+threads.append(monitor1)
+threads.append(trader1)
+threads.append(trader2)
+
+# Wait for all threads to complete
+for t in threads:
+    t.join()
+print "Exiting Main Thread"
