399059ba7f2d24c3303f61839f3a74e4faff3788
Teemu Heittola <teemu.heittola@gmail.com>
2013-11-21 23:21:40 +0200

    Implemented graceful shutdown

--- spiralbit.py
+++ spiralbit.py
@@ -1,3 +1,5 @@
+import signal
+import sys
 import json
 import urllib
 import urllib2
@@ -6,25 +8,40 @@ import threading
 import ConfigParser
 import hmac
 
+threads = []
+
 class trader (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.running = False
     def run(self):
+        self.running = True
         print "Starting " + self.name
         #print_time(self.name, self.counter, 5)
+        while self.running == True:
+            doTrading()
+            time.sleep(5)
         print "Exiting " + self.name
+    def stop(self):
+        self.running = False
 
 class monitor (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.running = False
     def run(self):
+        self.running = True
         print "Starting " + self.name
-        pullPrice()
+        while self.running == True:
+            pullPrice()
+            time.sleep(10)
         print "Exiting " + self.name
+    def stop(self):
+        self.running = False
 
 def ConfigSectionMap(section):
     dict1 = {}
@@ -39,14 +56,16 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
+def doTrading():
+    print "Trading..."
+
 def pullPrice():
-    while True:
-        url = "https://www.bitstamp.net/api/ticker"
-        #data = urllib.urlencode(parameters)
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        price = json.load(response)
-        time.sleep(10)
+    print "Pulling price..."
+    url = "https://www.bitstamp.net/api/ticker"
+    #data = urllib.urlencode(parameters)
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    price = json.load(response)
 
 def sign(signingnonce):
     message = signingnonce + client_id + api_key
@@ -64,6 +83,17 @@ def getBalance():
     balance = response.read()
     return balance
 
+def signalHandler(signal, frame):
+    print 'Closing...'
+    for t in threads:
+        t.stop()
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+    print "Bye"
+    sys.exit(0)
+
 nonce = 1
 Config = ConfigParser.ConfigParser()
 Config.read("config.ini")
@@ -73,7 +103,6 @@ api_key = ConfigSectionMap("Authentication")['key']
 API_SECRET = ConfigSectionMap("Authentication")['secret']
 
 threadLock = threading.Lock()
-threads = []
 
 # Create new threads
 monitor1 = monitor(1, "Monitor-1")
@@ -81,18 +110,19 @@ trader1 = trader(2, "Trader-1")
 trader2 = trader(3, "Trader-2")
 
 # Start new Threads
-monitor1.start()
-trader1.start()
-trader2.start()
-
-
+#monitor1.start()
+#trader1.start()
+#trader2.start()
 
 # Add threads to thread list
 threads.append(monitor1)
 threads.append(trader1)
 threads.append(trader2)
 
-# Wait for all threads to complete
+# Let's start
 for t in threads:
-    t.join()
-print "Exiting Main Thread"
+    t.start()
+
+# Keep running until users presses Ctrl+C
+signal.signal(signal.SIGINT, signalHandler)
+signal.pause()
