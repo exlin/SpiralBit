377504119880aaae5a536bf732548a19b436177e
Teemu Heittola <teemu.heittola@gmail.com>
2013-11-22 01:15:17 +0200

    Trader threads are now aware of latest price.

--- spiralbit.py
+++ spiralbit.py
@@ -5,102 +5,106 @@ import threading
 import ConfigParser
 import bitstamp
 
-threads = []
-
 class trader (threading.Thread):
-    def __init__(self, threadID, name):
+    def __init__(self, threadID, name, app):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.running = False
+        self.app = app
+        self.pollInterval = 5
+    
     def run(self):
         self.running = True
         print "Starting " + self.name
         #print_time(self.name, self.counter, 5)
         while self.running == True:
-            doTrading()
-            time.sleep(5)
+            if self.app.currentPrice > -1:
+                print "Trading with price " + str(self.app.currentPrice)
+            else:
+                print "Price not available."
+            time.sleep(self.pollInterval)
         print "Exiting " + self.name
+
     def stop(self):
         self.running = False
 
 class monitor (threading.Thread):
-    def __init__(self, threadID, name, pollInterval, api):
+    def __init__(self, threadID, name, app):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.running = False
-        self.pollInterval = pollInterval
-        self.api = api
+        self.pollInterval = 10
+        self.app = app
+    
     def run(self):
         self.running = True
         print "Starting " + self.name
         while self.running == True:
-            api.pullPrice()
-            time.sleep(10)
+            #Get the last price for trading.
+            self.app.currentPrice = self.app.api.pullPrice()['last']
+            time.sleep(self.pollInterval)
         print "Exiting " + self.name
+
     def stop(self):
         self.running = False
 
-def ConfigSectionMap(section):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                DebugPrint("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            dict1[option] = None
-    return dict1
+class App():
+    def __init__(self, api):
+        self.api = api
+        self.threadLock = threading.Lock()
+        self.currentPrice = -1
+        self.threads = []
+        self.threads.append(monitor(1, "Monitor-1", self))
+        self.threads.append(trader(2, "Trader-1", self))
+        self.threads.append(trader(3, "Trader-2", self))
 
-def doTrading():
-    print "Trading..."
+    def start(self):
+        for t in self.threads:
+            t.start()
 
+    def stop(self):
+        for t in self.threads:
+            t.stop()
+        for t in self.threads:
+            t.join()
 
+class Config():
+    def __init__(self, file):
+        cfgParser = ConfigParser.ConfigParser()
+        cfgParser.read("config.ini")
+
+        self.cid = self.ConfigSectionMap(cfgParser, "Authentication")['cid']
+        self.api_key = self.ConfigSectionMap(cfgParser, "Authentication")['key']
+        self.API_SECRET = self.ConfigSectionMap(cfgParser, "Authentication")['secret']
+        self.apiUrl = "https://www.bitstamp.net/api/"
+
+    def ConfigSectionMap(self, parser, section):
+        dict1 = {}
+        options = parser.options(section)
+        for option in options:
+            try:
+                dict1[option] = parser.get(section, option)
+                if dict1[option] == -1:
+                    DebugPrint("skip: %s" % option)
+            except:
+                print("exception on %s!" % option)
+                dict1[option] = None
+        return dict1
+
+# Configure and init the app.
+cfg = Config("config.ini")
+api = bitstamp.Bitstamp(cfg.apiUrl, cfg.cid, cfg.api_key, cfg.API_SECRET)
+app = App(api)
 
 def signalHandler(signal, frame):
     print 'Closing...'
-    for t in threads:
-        t.stop()
-
-    # Wait for all threads to complete
-    for t in threads:
-        t.join()
+    app.stop()
     print "Bye"
     sys.exit(0)
 
-Config = ConfigParser.ConfigParser()
-Config.read("config.ini")
-
-cid = ConfigSectionMap("Authentication")['cid']
-api_key = ConfigSectionMap("Authentication")['key']
-API_SECRET = ConfigSectionMap("Authentication")['secret']
-apiUrl = "https://www.bitstamp.net/api/"
-
-threadLock = threading.Lock()
-api = bitstamp.Bitstamp(apiUrl, cid, api_key, API_SECRET)
-
-# Create new threads
-monitor1 = monitor(1, "Monitor-1", 10, api)
-trader1 = trader(2, "Trader-1")
-trader2 = trader(3, "Trader-2")
-
-# Start new Threads
-#monitor1.start()
-#trader1.start()
-#trader2.start()
-
-# Add threads to thread list
-threads.append(monitor1)
-threads.append(trader1)
-threads.append(trader2)
-
-# Let's start
-for t in threads:
-    t.start()
-
-# Keep running until users presses Ctrl+C
+# Keep running until user presses Ctrl+C
+app.start()
 signal.signal(signal.SIGINT, signalHandler)
 signal.pause()
