eec4cb6752b02e3f190ebe24522d700a92556cb9
Henri Hirvonen <henri.hirvonen@wunderkraut.com>
2013-11-21 22:03:50 +0200

    Configurations

--- spiralbit.py
+++ spiralbit.py
@@ -3,6 +3,7 @@ import urllib
 import urllib2
 import time
 import threading
+import ConfigParser
 
 class trader (threading.Thread):
     def __init__(self, threadID, name):
@@ -24,6 +25,19 @@ class monitor (threading.Thread):
         pullPrice()
         print "Exiting " + self.name
 
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
 def pullPrice():
     while True:
         url = "https://www.bitstamp.net/api/ticker"
@@ -33,13 +47,35 @@ def pullPrice():
         price = json.load(response)
         time.sleep(10)
 
+def sign(signingnonce):
+    message = signingnonce + client_id + api_key
+    signature = hmac.new(API_SECRET, msg=message, digestmod=hashlib.sha256).hexdigest().upper()
+
+def getBalance():
+    balanceUrl = "https://www.bitstamp.net/api/balance"
+    parameters = {"key": api_key,
+    "signature": sign(nonce),
+    "nonce": nonce}
+    data = urllib.urlencode(parameters)
+    req = urllib2.Request(balanceUrl, data)
+    response = urllib2.urlopen(req)
+    json_object = response.read()
+
+nonce = 1
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+cid = ConfigSectionMap("Authentication")['cid']
+api_key = ConfigSectionMap("Authentication")['key']
+API_SECRET = ConfigSectionMap("Authentication")['secret']
+
 threadLock = threading.Lock()
 threads = []
 
 # Create new threads
 monitor1 = monitor(1, "Monitor-1")
-trader1 = trader(1, "Trader-1")
-trader2 = trader(2, "Trader-2")
+trader1 = trader(2, "Trader-1")
+trader2 = trader(3, "Trader-2")
 
 # Start new Threads
 monitor1.start()
